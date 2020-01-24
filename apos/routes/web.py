c42effce8ae4b6cb84b3cf2321b38e88d6a9998e
Timon Engelke <timon.engelke@online.de>
2020-01-24 15:51:38 +0100

    update routes to api definition

--- apos/routes/web.py
+++ apos/routes/web.py
@@ -1,7 +1,23 @@
 """Web Routes."""
 
-from masonite.routes import Get, Post
+from masonite.routes import Get, Post, Put, Patch, Delete
 
 ROUTES = [
-    Get('/', 'WelcomeController@show').name('welcome'),
+    Post('/api/v1/auth', 'AuthController@show'),
+
+    Get('/api/v1/orders', 'OrderController@list'),
+    Put('/api/v0/orders', 'OrderController@create'),
+
+    Get('/api/v1/orders/@id', 'OrderController@get'),
+    Patch('/api/v1/orders/@id', 'OrderController@edit'),
+    Delete('/api/v1/orders/@id', 'OrderController@delete'),
+
+    Put('/api/v1/orders/@order/items', 'ItemController@create'),
+    Get('/api/v1/orders/@order/items/@id', 'ItenController@get'),
+    Patch('/api/v1/orders/@order/items/@id', 'ItenController@edit'),
+    Delete('/api/v1/orders/@order/items/@id', 'ItenController@delete'),
+
+    Get('/coupons', 'CouponController@list'),
+    Put('/coupons', 'CouponController@create'),
+    Delete('/coupons/@id', 'CouponController@delete'),
 ]
