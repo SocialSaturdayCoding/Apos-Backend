a38424ddfed8d887b721ceb3f19f7a369269b406
Kim Wittenburg <codello@wittenburg.kim>
2020-03-08 15:50:21 +0100

    make psycopg2 dependency optional

--- config.example.py
+++ config.example.py
@@ -2,8 +2,6 @@ from config_base import ConfigBase
 
 
 class Config(ConfigBase):
-    DEBUG = True
-    TESTING = False
     SECRET_KEY = 'THIS IS A KEY THAT SHOULD BE CHANGED IN PRODUCTION'
     JWT_SECRET_KEY = 'THIS IS ANOTHER KEY THAT SHOULD BE CHANGED IN PRODUCTION'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./db.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pizza:pizza@localhost/pizza'
---
+++ config_base.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class ConfigBase(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
