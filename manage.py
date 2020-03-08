bc4b66b5d3a46e7507cb90f040e6294105d45ab0
Kim Wittenburg <codello@wittenburg.kim>
2020-03-08 18:25:22 +0100

    add test command

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 
+import pytest
 from flask_migrate import Migrate, Manager, MigrateCommand
 
 from apos.extensions import app, db
@@ -12,6 +13,12 @@ migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    pytest.cmdline.main(['-q'])
+
 
 if __name__ == '__main__':
     manager.run()
