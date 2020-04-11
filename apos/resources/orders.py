c8e7f8a60358f3fabf173937638458e7b16624d0
Florian Vahl <florian@flova.de>
2020-04-11 15:08:32 +0200

    Fix typo

--- apos/resources/orders.py
+++ apos/resources/orders.py
@@ -38,7 +38,6 @@ class OrderListResource(Resource):
     @jwt_required
     def put(self):
         args = parsers['normal']['parser'].parse_args()
-        if 
         order = Order(owner_id=get_jwt_identity(), **args)
         db.session.add(order)
         db.session.commit()
